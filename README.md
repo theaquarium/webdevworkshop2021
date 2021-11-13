9009fd896a8d037c1a06b1112acc26e1ef18a1ff
Leah Vashevko <leah@vashevko.com>
2021-11-12 22:36:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Webdev Workshop: Advanced HTML, CSS, and JS
+Sample code from this workshop.
+
+## Accompanying slideshow
+<https://docs.google.com/presentation/d/1AbnQ58CK8PgX7saACZYPJSZB3ApgUtNu2KFXjVWwlp8/edit?usp=sharing>
---
+++ main.js
@@ -0,0 +1,32 @@
+const totalPrice = document.querySelector('#price');
+const totalItems = document.querySelector('#total-count');
+
+const itemPrices = {
+    'apple': 2,
+    'banana': 4,
+    'orange': 1,
+}
+let itemsList = [];
+
+document.querySelectorAll('.item').forEach(function(itemCard) {
+    const itemType = itemCard.dataset.item;
+
+    itemCard.querySelector('.item-button > button').addEventListener('click', function() {
+        itemsList.push(itemType);
+
+        const numItems = itemsList.length;
+        if (numItems === 0) {
+            totalItems.innerHTML = 'nothing';
+        } else if (numItems === 1) {
+            totalItems.innerHTML = '1 item';
+        } else {
+            totalItems.innerHTML = `${numItems} items`;
+        }
+
+        let price = 0;
+        itemsList.forEach(function(item) {
+            price += itemPrices[item];
+        });
+        totalPrice.innerHTML = `$${price}`;
+    })
+});
